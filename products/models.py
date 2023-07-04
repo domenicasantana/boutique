058c73d564230f9765dba846c675256166e37b05
domenicasantana <dasc1996@hotmail.com>
2023-07-04 20:42:05 +0000

    Customized admin

--- products/models.py
+++ products/models.py
@@ -3,10 +3,14 @@ from django.db import models
 # Create your models here.
 
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
-    def _str_(self):
+    def __str__(self):
         return self.name
 
     def get_friendly_name(self):
@@ -22,5 +26,5 @@ class Product(models.Model):
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
-    def _str_(self):
+    def __str__(self):
         return self.name
